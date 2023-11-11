3271d5b0766f716609ae8cd8789d1973b735f187
cjacobs8587 <cbj225@lehigh.edu>
2023-11-11 15:37:56 -0500

    calculate distance

--- src/calculator.py
+++ src/calculator.py
@@ -1,13 +1,14 @@
 import boto3
 
+# Initialize the AWS Location Service client
 client = boto3.client('location', region_name='us-east-2')
 
-place_index = 'SafeWalk'  
-calculator_name = 'SafeWalk' 
+place_index = 'SafeWalk'
+calculator_name = 'SafeWalk'
 
 # Specify the start and end addresses
-start_address = '405 Selfridge StBethlehem, PA 18015'  
-end_address = '201 E Packer Ave, Bethlehem, PA 18015'  
+start_address = '405 Selfridge StBethlehem, PA 18015'
+end_address = '201 E Packer Ave, Bethlehem, PA 18015'
 
 # Function to geocode an address
 def geocode_address(client, place_index, address):
@@ -22,7 +23,6 @@ start_latitude, start_longitude = geocode_address(client, place_index, start_add
 end_latitude, end_longitude = geocode_address(client, place_index, end_address)
 
 
-
 # Make the API call to calculate the route
 route_response = client.calculate_route(
     CalculatorName=calculator_name,
@@ -31,10 +31,21 @@ route_response = client.calculate_route(
     TravelMode='Walking'  
 )
 
-# Print the response
+# Debugging: Print the raw route response
+print("Raw Route Response:")
 print(route_response)
 
+# Function to print a detailed description of the route
+def describe_route(route_response):
+    print("Route Description:")
+    for leg in route_response.get('Legs', []):
+        for step in leg.get('Steps', []):
+            # Format distance as a string with two decimal places
+            distance_text = "{:.2f} meters".format(step['Distance'] * 1000)  # Assuming distance is in kilometers
+            # The instruction is not provided in the response, so we use start and end positions
+            instruction = "From {} to {}".format(step['StartPosition'], step['EndPosition'])
+            print(distance_text + ": " + instruction)
 
-
-
+# Print the detailed route description
+describe_route(route_response)
 
